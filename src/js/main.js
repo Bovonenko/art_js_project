399aa6f660b800639327f3fcfecfd8dd9d6f444c
Rostyslav <bovonenko@icloud.com>
2022-06-25 16:41:41 +0200

    scroll made with JS

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,7 @@ import filter from './modules/filter';
 import pictureSize from './modules/pictureSize';
 import accordion from './modules/accordion';
 import burger from './modules/burger';
+import scrolling from './modules/scrolling';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -35,4 +36,5 @@ window.addEventListener('DOMContentLoaded', () => {
     pictureSize('.sizes-block');
     accordion('.accordion-heading');
     burger('.burger-menu', '.burger');
+    scrolling('.pageup');
 });
